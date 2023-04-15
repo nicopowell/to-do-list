cb922e658e6be6af9338175516bb0879a228d9b8
Nicolas Powell <nicopowell30@gmail.com>
2023-04-15 17:30:46 -0300

    optimice el codigo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
+// Selecciona los elementos del DOM al principio del código
 let formulario = document.querySelector("form");
 let contenedorTareas = document.getElementById("contenedorTareas");
 let spanTareasPendientes = document.getElementById("tareasPendientes");
 let btnBorrarTodo = document.getElementById('btnBorrarTodo');
 
-formulario.addEventListener("submit", agregarTarea);
-btnBorrarTodo.addEventListener('click', borrarTodo)
-
+// Define las funciones antes de usarlas
 function agregarTarea(e) {
   e.preventDefault();
   let tarea = document.getElementById("inputTarea").value.trim();
@@ -16,33 +15,28 @@ function agregarTarea(e) {
   }
 
   let nuevaTarea = document.createElement("li");
-  nuevaTarea.className = "rounded d-flex justify-content-between";
+  nuevaTarea.classList.add("rounded", "d-flex", "justify-content-between");
   
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.className = "form-check-input m-0"
-  checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      textoTarea.classList.add("completada");
-    } else {
-      textoTarea.classList.remove("completada");
-    }
-    tareasPendientes(); // Llama a la función tareasPendientes al cambiar el checkbox
+  checkbox.classList.add("form-check-input", "m-0")
+  checkbox.addEventListener("change", () => {
+    textoTarea.classList.toggle("completada");
+    tareasPendientes();
   });
   
   nuevaTarea.appendChild(checkbox);
   
   let textoTarea = document.createElement("span");
   textoTarea.textContent = tarea;
-  textoTarea.className = "flex-grow-1 ms-3"
+  textoTarea.classList.add("flex-grow-1", "ms-3")
   nuevaTarea.appendChild(textoTarea);
   
   let botonBorrar = document.createElement("button");
-  botonBorrar.className = "btn p-0";
+  botonBorrar.classList.add("btn", "p-0");
   botonBorrar.innerHTML = '<i class="bi bi-trash fs-5 px-0"></i>';
-  botonBorrar.addEventListener("click", function () {
+  botonBorrar.addEventListener("click", () => {
     nuevaTarea.remove();
-    tareasPendientes--; // Disminuye tareasPendientes al borrar una tarea
     tareasPendientes();
   });
   
@@ -52,20 +46,16 @@ function agregarTarea(e) {
   tareasPendientes();
 }
 
-function borrarTarea(boton) {
-  let tarea = boton.parentNode;
-  tarea.remove();
-  tareasPendientes();
-}
-
 function tareasPendientes() {
   let tareasPendientes = contenedorTareas.querySelectorAll("input[type='checkbox']:not(:checked)").length;
-  spanTareasPendientes.innerHTML = tareasPendientes;
+  spanTareasPendientes.textContent = tareasPendientes;
 }
 
 function borrarTodo() {
-  while (contenedorTareas.firstChild) {
-    contenedorTareas.removeChild(contenedorTareas.firstChild);
-  }
+  contenedorTareas.innerHTML = "";
   tareasPendientes();
 }
+
+// Asigna los eventos al final del código
+formulario.addEventListener("submit", agregarTarea);
+btnBorrarTodo.addEventListener('click', borrarTodo);
